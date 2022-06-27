0fbb9fa17b8112c83a82e70431e96b96f83f472c
unknown <riic.nun@gmail.com>
2022-06-27 18:44:47 -0300

    Finish

---
+++ .slicemachine/assets/slices/MyPost/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/MyPost';
+
+export default {
+  title: 'slices/MyPost'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"my_post","items":[],"primary":{"title":[{"type":"heading1","text":"Innovate cross-media convergence","spans":[]}],"subtitle":[{"type":"paragraph","text":"Ut mollit consequat ex. Eiusmod consequat enim Lorem amet.","spans":[]}],"author":"maximize seamless infrastructures","banner":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?w=900&h=500&fit=crop"}},"id":"_Default"}} />
+_Default.storyName = 'Default'
